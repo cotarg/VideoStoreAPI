ec5f676757fd726d687e6da6c366edfb226afa80
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-15 15:34:19 -0700

    added more comments to controllers to help us later

--- controllers/rental_controller.js
+++ controllers/rental_controller.js
@@ -3,6 +3,7 @@ var RentalController = {
     res.render('index',  { title: ""})
   },
 
+  // See a list of customers that have currently checked out any of the movie's inventory (/rentals/Jaws/customers)
   customersRentingThisFilm:  function(req, res){
     res.render('index',  { title: ""})
   },
@@ -15,6 +16,10 @@ var RentalController = {
     res.render('index',  { title: ""})
   },
 
+  // Given a customer's id and a movie's title ...
+  // "check out" one of the movie's inventory to the customer (/rentals/Jaws/check-out)
+  // Establish a return date
+  // Charge the customer's account (cost up to you)
   checkoutFilmToCust:  function(req, res){
     res.render('index',  { title: ""})
   },
@@ -31,3 +36,17 @@ var RentalController = {
 }
 
 module.exports = RentalController
+
+// Look a movie up by title to see (/rentals/Jaws)
+// it's synopsis
+// release date
+// available inventory (not currently checked-out to a customer)
+// and inventory total
+// Given a customer's id and a movie's title ...
+// "check out" one of the movie's inventory to the customer (/rentals/Jaws/check-out)
+// Establish a return date
+// Charge the customer's account (cost up to you)
+// "check in" one of customer's rentals (/rentals/Jaws/return)
+// return the movie to its inventory
+// See a list of customers with overdue movies (/rentals/overdue)
+// include customer name, movie title, check-out date, and return date
