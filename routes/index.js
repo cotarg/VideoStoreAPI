2198be797ae1d1f9c24d89ae2791e6f1cdf25e70
Rowan Cota <cota.rg@gmail.com>
2016-06-15 15:33:47 -0700

    working on seeding.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
 var express = require('express')
 var router = express.Router()
 var routes = {
-  list_all_customers: '/customers',
-  customers_sorted_by_name: '/customers/sort/name',
-  customers_sorted_by_zip: '/customers/sort/postal_code',
-  list_customer_checkouts: '/customers/:id/current',
-  list_customer_history: '/customers/:id/history',
-  list_all_movies: '/movies',
-  movies_sorted_by_title: '/movies/sort/title',
-  movies_sorted_by_release: '/movies/sort/release_date',
-  movie_details: '/movies/:title',
-  movie_checkouts: '/movies/:title/current',
-  movie_hist_name_sort: '/movies/:title/history/sort/name',
-  movie_hist_checkout_sort: '/movies/:title/history/sort/date',
-  rental_checkouts: '/rentals/:title/current',
-  rental_hist_name_sort: '/rentals/:title/history/sort/name',
-  rental_hist_checkout_sort: '/rentals/:title/history/sort/date',
-  checkout_film_to_cust: '/rentals/:title/checkout',
-  checkin_film_from_cust: '/rentals/:title/return',
-  list_of_overdue_films: '/rentals/overdue'
+  list_all_customers: 'http://customers',
+  customers_sorted_by_name: 'http://customers/sort/name',
+  customers_sorted_by_zip: 'http://customers/sort/postal_code',
+  list_customer_checkouts: 'http://customers/:id/current',
+  list_customer_history: 'http://customers/:id/history',
+  list_all_movies: 'http://movies',
+  movies_sorted_by_title: 'http://movies/sort/title',
+  movies_sorted_by_release: 'http://movies/sort/release_date',
+  movie_details: 'http://movies/:title',
+  movie_checkouts: 'http://movies/:title/current',
+  movie_hist_name_sort: 'http://movies/:title/history/sort/name',
+  movie_hist_checkout_sort: 'http://movies/:title/history/sort/date',
+  rental_checkouts: 'http://rentals/:title/current',
+  rental_hist_name_sort: 'http://rentals/:title/history/sort/name',
+  rental_hist_checkout_sort: 'http://rentals/:title/history/sort/date',
+  checkout_film_to_cust: 'http://rentals/:title/checkout',
+  checkin_film_from_cust: 'http://rentals/:title/return',
+  list_of_overdue_films: 'http://rentals/overdue'
 }
 
 /* GET home page. */
