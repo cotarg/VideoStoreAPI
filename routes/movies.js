1d1bd76fc5822d4c1950c9a342f092f3253827fc
Rowan Cota <cota.rg@gmail.com>
2016-06-16 14:08:08 -0700

    removing excess spaces and fixing formatting.

--- routes/movies.js
+++ routes/movies.js
@@ -1,14 +1,18 @@
+var express = require('express')
+var router = express.Router()
+
 // movies index
-router.get ('/movies' , 'movies_controller.listAllMovies')
+router.get('/movies', 'movies_controller.listAllMovies')
 
 // movies sorted
-router.get ('/movies/sort/title' , 'movies_controller.moviesByTitle')
-router.get ('/movies/sort/release_date' , 'movies_controller.moviesByRelease')
+router.get('/movies/sort/title', 'movies_controller.moviesByTitle')
+router.get('/movies/sort/release_date', 'movies_controller.moviesByRelease')
 
 // look at individual titles
 // this requires a film title which is used in the HTTP request
-router.get ('/movies/:title' , 'movies_controller.seeMovieDetails')
+router.get('/movies/:title', 'movies_controller.seeMovieDetails')
+router.get('/movies/:title/current', 'movies_controller.customersRentingThisFilm')
+router.get('/movies/:title/history/sort/name', 'movies_controller.filmRentalHistoryByCustName')
+router.get('/movies/:title/history/sort/date', 'movies_controller.filmRentalHistoryByCODate')
 
-router.get ('/movies/:title/current' , 'movies_controller.customersRentingThisFilm')
-router.get ('/movies/:title/history/sort/name' , 'movies_controller.filmRentalHistoryByCustName')
-router.get ('/movies/:title/history/sort/date' , 'movies_controller.filmRentalHistoryByCODate')
+module.exports = router
