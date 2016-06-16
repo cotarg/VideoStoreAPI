850bc0e970bbf0329fdf639b033a739b3ddd8083
Rowan Cota <cota.rg@gmail.com>
2016-06-16 14:01:41 -0700

    added models dir.

--- routes/customers.js
+++ routes/customers.js
@@ -1,18 +1,17 @@
-var express = require('express');
-var router = express.Router();
-var customers_controller = require('../controllers/customers_controller')
-
+var express = require('express')
+var router = express.Router()
+var customersController = require('../controllers/customers_controller')
 
 // customers index
-router.get('/' , customers_controller.customerList)
+router.get('/', customersController.customerList)
 
 // customers sort
-router.get('/sort/name' , customers_controller.customersNameSort)
-router.get('/sort/registered_at' , customers_controller.customersRegisteredSort)
-router.get('/sort/postal_code' , customers_controller.customersPostalSort)
+router.get('/sort/name', customersController.customersNameSort)
+router.get('/sort/registered_at', customersController.customersRegisteredSort)
+router.get('/sort/postal_code', customersController.customersPostalSort)
 
 // customers by id
-router.get('/:id/current' , customers_controller.currentCheckOuts)
-router.get('/:id/history' , customers_controller.historicalCheckOuts)
+router.get('/:id/current', customersController.currentCheckOuts)
+router.get('/:id/history', customersController.historicalCheckOuts)
 
 module.exports = router
