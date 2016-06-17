02eb576d772401bda287b9fdcefa6560cc8ed786
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-17 13:30:06 -0400

    got query stuff working for limit for customer sort

--- controllers/customers_controller.js
+++ controllers/customers_controller.js
@@ -14,17 +14,32 @@ var CustomersController = {
 
   // Retrive a subset of customers (/customers/sort/name?n=10&p=2)
   // Given a sort column, return n customer records, offset by p records (this will be used to create "pages" of customers)
+  // db.products.find({},{order: "price desc"}, function(err,products){
+  //   //products ordered in descending fashion
+  // });
   customersNameSort:  function(req, res){
-    res.render('index',  { title: ""})
+    var db = req.app.get('db')
+    var num = Number(req.query.n)
+    db.customers.find({}, {order: 'name asc', limit: num}, function(err, result){
+      res.json(result)
+    });
   },
 
   //sort by registered_at
   customersRegisteredSort:  function(req, res){
-    res.render('index',  { title: ""})
+    var db = req.app.get('db')
+    var num = Number(req.query.n)
+    db.customers.find({}, {order: 'registered_at asc', limit: num}, function(err, result){
+      res.json(result)
+    });
   },
 
   customersPostalSort:  function(req, res){
-    res.render('index',  { title: ""})
+    var db = req.app.get('db')
+    var num = Number(req.query.n)
+    db.customers.find({}, {order: 'postal_code asc', limit: num}, function(err, result){
+      res.json(result)
+    });
   },
 
   // Given a customer's id...
@@ -32,6 +47,8 @@ var CustomersController = {
   // includes return date
   // ordered by check out date
   currentCheckOuts:  function(req, res){
+    var db = req.app.get('db')
+
     res.render('index',  { title: ""})
   },
 
@@ -40,6 +57,8 @@ var CustomersController = {
   // includes return date
   // ordered by check out date
   historicalCheckOuts:  function(req, res){
+    var db = req.app.get('db')
+
     res.render('index',  { title: ""})
   }
 
