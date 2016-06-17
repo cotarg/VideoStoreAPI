69c99328263f6f31430ddc542840683e0f29b732
Rowan Cota <cota.rg@gmail.com>
2016-06-17 13:36:19 -0700

    working on rentals savesync.

--- models/rental.js
+++ models/rental.js
@@ -11,18 +11,32 @@ var Rental = function(id) {
 
 
 // Class Functions
-// request is currently placeholding while I figure out
+// placeholder is currently placeholding while I figure out
 // how to build an object or wevs from params
-Rental.create = function(request, callback) {
+Rental.create = function (placeholder, callback) {
   db.accounts.save({
-    balance: initialBalance
-  }, function(error, account) {
-    if(error || !account) {
+    customer_id: placeholder.customer_id,
+    movie_id: placeholder.movie_id,
+    due_date: placeholder.due_date,
+    checkout_date: placeholder.checkout_date
+  }, function (error, account) {
+    if (error || !account) {
       callback(error || new Error("Could not create account"), undefined);
     } else {
-      callback(null, new Account(account.id));
+      callback(null, new Rental(rental.id));
     }
-  });
-};
+  })
+}
+
+Account.createSync = function(placeholder, callback) {
+  var account = db.accounts.save({
+    customer_id: placeholder.customer_id,
+    movie_id: placeholder.movie_id,
+    due_date: placeholder.due_date,
+    checkout_date: placeholder.checkout_date
+  }
+
+  return new Account(account.id);
+}
 
 module.exports = Rental;
