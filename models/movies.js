a19f12fc2cd984bb792f65ee198dcc91b8f58527
Rowan Cota <cota.rg@gmail.com>
2016-06-17 12:53:19 -0700

    renamed models to follow convention.

--- models/movies.js
+++
@@ -1,8 +0,0 @@
-var app = require("../app");
-var db = app.get("db");
-
-// Constructor (like in js-bank-accounts)
-// Constructor function
-var Movie = function(title) {
-  this.title = title;
-};
---
+++ models/rental.js
@@ -0,0 +1,16 @@
+var movie = require('../models/movies')
+var app = require("../app");
+var db = app.get("db");
+
+// Constructor function
+var Rental = function(id) {
+  this.id = id;
+};
+
+// Instance functions
+
+
+// Class Functions
+
+
+module.exports = Account;
