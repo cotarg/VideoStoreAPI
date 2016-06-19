e282ebcbe97618565f6b04cb9bd16cd8500ab2b3
Rowan Cota <cota.rg@gmail.com>
2016-06-19 11:09:35 -0700

    rental checkin edited.

--- models/rental.js
+++ models/rental.js
@@ -18,7 +18,7 @@ Rental.return = function (callback) {
     if(error) {
       callback(error, undefined);
     } else {
-      db.rentals.save({returned_date: return_date})
+      db.rentals.save({id: this.id, returned_date: return_date})
     }
   })
 
