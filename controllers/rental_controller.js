d61bbfd71377799e9851a2e62e84445c503576ec
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-20 15:29:20 -0700

    method updated to included upping inventory when movie gets returned

--- controllers/rental_controller.js
+++ controllers/rental_controller.js
@@ -55,13 +55,11 @@ var RentalController = {
     db.movies.where('title = $1', [title], function(err, result) {
       var stock = result[0].stock - 1
       db.movies.save({id: result[0].id, stock: stock }, function(err, res){
-
       })
       db.customers.find({id: id}, function(err, result){
         credit = result[0].account_credit - rental_cost
         db.customers.save({id: result[0].id, account_credit: credit }, function(err, res){})
       });
-    // find instance of that movie in database so we can get its inventory and movie_id
       db.rentals.save({movie_id: result[0].id, title: title, customer_id: id, checkout_date: today, due_date: due_date}, function(err, inserted){
         res.json(title)
       })
@@ -76,8 +74,10 @@ var RentalController = {
     var today = new Date()
 
     db.rentals.where('title = $1 AND customer_id = $2', [title, id], function(err, result) {
-      console.log('damn')
-    // find instance of that movie in database so we can get its inventory and movie_id
+      db.movies.find({title: title}, function(err, result){
+        var stock = result[0].stock + 1
+        db.movies.save({id: result[0].id, stock: stock }, function(err, res){})
+      });
       db.rentals.save({id: result[0].id, returned_date: today}, function(err, inserted){
         res.json(title)
       })
